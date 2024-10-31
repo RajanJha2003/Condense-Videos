ed7c54df71a8cb3fd252d189bc6ec0fb21590912
Rajan <rajanj448@student.sfit.ac.in>
2024-10-31 11:07:59 +0530

    Hero compoent and Public images

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Hero from "@/components/hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="max-w-5xl mx-auto pt-32 space-y-16 sm:space-y-32">
+      <div>
+        <Hero />
+        
+      </div>
 
     </div>
   );
